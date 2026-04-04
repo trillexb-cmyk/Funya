7d14c9688060100a84b84ae6d07d9bfcc3551a9c
trillexb-cmyk <trillexb@gmail.com>
2026-04-05 00:55:35 +0300

    Delete handlers directory

--- handlers/init.py
+++
@@ -1,5 +0,0 @@
-from . import start
-from . import private
-from . import actions
-from . import modules
-from . import chat
--- handlers/start.py
+++
@@ -1,5 +0,0 @@
-from loader import bot
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    bot.send_message(message.chat.id, "👋 Привет!\nНапиши: фуня помоги")
