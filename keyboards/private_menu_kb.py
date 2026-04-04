09de0cc897ac3b0467dbf990955726420dccaeda
trillexb-cmyk <trillexb@gmail.com>
2026-04-05 00:54:12 +0300

    Delete keyboards directory

--- keyboards/private_menu_kb.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import ReplyKeyboardMarkup
-
-def menu():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.row("👤 Профиль", "💎 Баланс")
-    kb.row("🎁 Бонус", "🎒 Инвентарь")
-    kb.row("🛒 Магазин", "❤️ Отношения")
-    return kb
