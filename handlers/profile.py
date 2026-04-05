760380513c240194b25d663372b6ba833ca39361
trillexb-cmyk <trillexb@gmail.com>
2026-04-05 12:50:59 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,31 +1,16 @@
-from database import get_user, add_user
+from database import add_user, get_user
 
 
 def show_profile(bot, message):
     user_id = message.from_user.id
 
+    add_user(user_id)
     user = get_user(user_id)
 
-    if not user:
-        add_user(user_id)
-        user = get_user(user_id)
-
-    balance = user[1]
-    cookies = user[2]
-    clan = user[3]
-    partner = user[4]
-
-    text = (
+    bot.send_message(
+        message.chat.id,
         f"👤 Профиль\n\n"
-        f"🆔 Айди: {user_id}\n"
-        f"💰 Баланс: {balance} 🍬Фунтиков\n"
-        f"🍪 Печенек: {cookies}\n"
-        f"⚒ Характеристика: 0\n"
+        f"🆔 {user_id}\n"
+        f"💰 Баланс: {user[1]}\n"
+        f"🍪 Печеньки: {user[2]}"
     )
-
-    if partner:
-        text += f"💑 Пара: {partner}\n"
-
-    text += f"🏰 Клан: {clan}\n"
-
-    bot.send_message(message.chat.id, text)
