7349122a8f39af36b8b19e19a6fc9d81711a959f
trillexb-cmyk <trillexb@gmail.com>
2026-04-05 12:48:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 import telebot
 import time
-import random
 
-from config import TOKEN, ADMIN_ID
-
-import handlers.menu as menu
+from config import TOKEN
 import handlers.router as router
-
-from database import reset_db
 
 bot = telebot.TeleBot(TOKEN)
 
 
-# ===== СТАРТ =====
 @bot.message_handler(commands=['start'])
-def start(msg):
-    menu.send_menu(bot, msg.chat.id)
+def start(message):
+    router.start(bot, message)
 
 
-# ===== ОБЩИЙ ОБРАБОТЧИК =====
 @bot.message_handler(func=lambda message: True)
 def handler(message):
-    router.handle_all(bot, message)
+    router.handle(bot, message)
 
 
 print("Бот запущен...")
 
-
-# ===== ФИКС 409 =====
 bot.remove_webhook()
 
-
-# ===== ЗАПУСК =====
 while True:
     try:
         bot.infinity_polling(skip_pending=True)
     except Exception as e:
         print("Ошибка:", e)
-        time.sleep(5)
+        time.sleep(3)
