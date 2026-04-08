0c7865865eafb46510ba8894c2d4c8430cfb3e97
trillexb-cmyk <trillexb@gmail.com>
2026-04-08 21:55:21 +0300

    Update balance.py

--- modules/balance.py
+++ modules/balance.py
@@ -1,2 +1,23 @@
+from database import add_user, get_user
+
+
 def run(bot, message):
-    bot.send_message(message.chat.id, "💰 Баланс\n\n🚧 В разработке")
+    user_id = message.from_user.id
+
+    add_user(user_id)
+    user = get_user(user_id)
+
+    if not user:
+        bot.send_message(message.chat.id, "Ошибка баланса")
+        return
+
+    balance = user["balance"]
+    cookies = user["cookies"]
+
+    text = (
+        f"💰 Баланс\n\n"
+        f"💵 Деньги: {balance}\n"
+        f"🍪 Печеньки: {cookies}"
+    )
+
+    bot.send_message(message.chat.id, text)
