0307208b1f27b9ff79b1cda2a347cb3aeeac8be9
trillexb-cmyk <trillexb@gmail.com>
2026-04-08 21:32:27 +0300

    Update profile.py

--- modules/profile.py
+++ modules/profile.py
@@ -1,2 +1,44 @@
+from database import add_user, get_user
+
+
 def run(bot, message):
-    bot.send_message(message.chat.id, "👤 Профиль\n\n🚧 В разработке")
+    user_id = message.from_user.id
+
+    add_user(user_id)
+    user = get_user(user_id)
+
+    if not user:
+        bot.send_message(message.chat.id, "Ошибка профиля")
+        return
+
+    (
+        uid,
+        balance,
+        cookies,
+        clan,
+        partner,
+        last_bonus,
+        exp,
+        level,
+        reputation,
+        messages,
+        warns,
+        mute_until
+    ) = user
+
+    text = (
+        f"👤 Профиль\n\n"
+        f"🆔 ID: {uid}\n"
+        f"💰 Баланс: {balance}\n\n"
+
+        f"🏆 Уровень: {level}\n"
+        f"⚡ Опыт: {exp}\n\n"
+
+        f"💬 Сообщения: {messages}\n"
+        f"⭐ Репутация: {reputation}\n\n"
+
+        f"👥 Клан: {clan}\n"
+        f"💍 Партнёр: {partner if partner else 'нет'}"
+    )
+
+    bot.send_message(message.chat.id, text)
